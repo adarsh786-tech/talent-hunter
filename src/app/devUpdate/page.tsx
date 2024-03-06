9cd51a4a2cd9e66457c075306bac798a5ef3a3f1
mihir <mihir.1si17cs058@gmail.com>
2024-03-06 19:17:41 +0530

    Updated

--- src/app/devUpdate/page.tsx
+++ src/app/devUpdate/page.tsx
@@ -1,32 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Logo from "@/assets/talenthunter.png";
 import Details from "@/app/devUpdate/Details";
 import CardUI from "@/app/devUpdate/CardUI";
 import Image from "next/image";
 import axios from "axios";
-
-// const getProjects = async () => {
-//   try {
-//     const response = await axios.get(
-//       // "http://localhost:3000/api/devprojects/projectupload"
-//       `${process.env.BASE_API_URL}/api/devprojects/projectupload`
-//     );
-//     console.log(response.data);
-//     return response.data;
-//   } catch (error) {
-//     console.error("Error fetching projects:", error);
-//     throw error; // Optionally re-throw the error to handle it in the calling function
-//   }
-// };
-// // "http://localhost:3000/api/devprojects/projectupload"
-// interface Project {
-//   projectName: string;
-//   projectDescription: string;
-//   projectURL: string;
-//   projectStack: string;
-
-//   // Add other properties as needed
-// }
+import { GetStaticProps, GetServerSideProps } from "next";
 
 interface Project {
   projectName: string;
@@ -47,6 +25,12 @@ const getProjects = async () => {
     throw error; // Optionally re-throw the error to handle it in the calling function
   }
 };
+
+// useEffect(() => {
+// const response = await getProjects();
+// const projectList: Project[] = response.data;
+// }, []);
+
 const DevUpdate = async () => {
   if (!process.env.BASE_API_URL) {
     return null;
@@ -54,6 +38,9 @@ const DevUpdate = async () => {
 
   const response = await getProjects();
   const projectList: Project[] = response.data;
+  console.log("-----------------------------");
+  console.log(projectList.length);
+  console.log("-----------------------------");
 
   return (
     <div className="bg-gradient-to-br from-slate-700 via-slate-950 to-slate-700">
@@ -73,15 +60,25 @@ const DevUpdate = async () => {
         </div>
       </div>
       <div className="flex flex-row gap-4">
-        {projectList.map((project: Project, indx: number) => (
-          <CardUI
-            key={indx}
-            title={project.projectName}
-            description={project.projectDescription}
-            url={project.projectURL}
-            techStack={project.projectStack}
-          />
-        ))}
+        {projectList.map((project: Project, indx: number) => {
+          console.log(`
+          Index Value: ${indx},
+          Project Name: ${project.projectName},
+          Project Description: ${project.projectDescription},
+          Project URL: ${project.projectURL},
+          Project Tech Stack: ${project.projectStack},
+          \n
+          `);
+          return (
+            <CardUI
+              key={indx}
+              title={project.projectName}
+              description={project.projectDescription}
+              url={project.projectURL}
+              techStack={project.projectStack}
+            />
+          );
+        })}
       </div>
     </div>
   );
